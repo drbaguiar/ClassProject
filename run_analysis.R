12ba0415befd0b9a73d3c29d897480490dcd4b1b
Bryan Aguiar <drbaguiar@gmail.com>
2014-09-16 07:44:18 -0500

    This are changes

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,9 @@
 if (!require("data.table")) {
-        install.packages("data.table")
+  install.packages("data.table")
 }
 
 if (!require("reshape2")) {
-        install.packages("reshape2")
+  install.packages("reshape2")
 }
 
 require("data.table")
@@ -11,18 +11,20 @@ require("reshape2")
 
 #Check for the directory. If not there, download the data and extract it
 if (!file.exists("UCI HAR Dataset")) {
-zip.url <- 'http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
-zip.file <- 'dataset.zip'
-download.file(zip.url, destfile = zip.file)
-unzip(zip.file)
+  zip.url <- 'http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+  zip.file <- 'dataset.zip'
+  download.file(zip.url, destfile = zip.file)
+  unzip(zip.file)
 }
- 
+
 # Load the activity labels and the data column names
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
-features <- read.table("./UCI HAR Dataset/features.txt")[,2]
+features <- tolower(read.table("./UCI HAR Dataset/features.txt")[,2])
 
 # Extract only the measurements on the mean and standard deviation for each measurement.
 extract_features <- grepl("mean|std", features)
+extract_features <- gsub("\\()","",extract_features)
+extract_features <- gsub("-","",extract_features)
 
 # Load and process X_test & y_test data.
 X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
@@ -60,12 +62,13 @@ train_data <- cbind(as.data.table(subject_train), y_train, X_train)
 
 # Merge test and train data
 data = rbind(test_data, train_data)
-
 id_labels   = c("subject", "Activity_ID", "Activity_Label")
 data_labels = setdiff(colnames(data), id_labels)
+
 melt_data      = melt(data, id = id_labels, measure.vars = data_labels)
 
 # Apply mean function to dataset using dcast function
 tidy_data   = dcast(melt_data, subject + Activity_Label ~ variable, mean)
 
 write.table(tidy_data, file = "./tidydata.txt")
+data
