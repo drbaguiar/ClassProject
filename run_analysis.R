84c6e0fd3fcaaa195dbd14b09230be736a710dd6
Dr.B <drbaguiar@gmail.com>
2014-11-01 22:49:49 -0500

    Udpated directory

--- run_analysis.R
+++ run_analysis.R
@@ -6,20 +6,18 @@ if (!require("reshape2")) {
   install.packages("reshape2")
 }
 
-require("data.table")
-require("reshape2")
 
 #Check for the directory. If not there, download the data and extract it
-if (!file.exists("UCI HAR Dataset")) {
+if (!file.exists("C:/Users/bryan_000/Documents/GitHub/Data/UCI HAR Dataset")) {
   zip.url <- 'http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
-  zip.file <- 'dataset.zip'
+  zip.file <- 'C:/Users/bryan_000/Documents/GitHub/Data/dataset.zip'
   download.file(zip.url, destfile = zip.file)
-  unzip(zip.file)
+  unzip("C:/Users/bryan_000/Documents/GitHub/Data/dataset.zip")
 }
 
 # Load the activity labels and the data column names. CLean data column names
-activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
-features <- read.table("./UCI HAR Dataset/features.txt")[,2]
+activity_labels <- read.table("C:/Users/bryan_000/Documents/GitHub/Data/UCI HAR Dataset/activity_labels.txt")[,2]
+features <- read.table("C:/Users/bryan_000/Documents/GitHub/Data/UCI HAR Dataset/features.txt")[,2]
 features <- tolower(features)
 features <- gsub("\\(","",features)
 features <- gsub("\\)","",features)
@@ -31,9 +29,9 @@ features <- gsub(",","",features)
 extract_features <- grepl("mean|std", features)
 
 # Load and process X_test & y_test data.
-X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
-subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+X_test <- read.table("C:/Users/bryan_000/Documents/GitHub/Data/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("C:/Users/bryan_000/Documents/GitHub/Data/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("C:/Users/bryan_000/Documents/GitHub/Data/UCI HAR Dataset/test/subject_test.txt")
 names(X_test) = features
 
 # Extract only the measurements on the mean and standard deviation for each measurement.
@@ -48,9 +46,9 @@ names(subject_test) = "subject"
 test_data <- cbind(as.data.table(subject_test), y_test, X_test)
 
 # Load and process X_train & y_train data.
-X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
-subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+X_train <- read.table("C:/Users/bryan_000/Documents/GitHub/Data/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("C:/Users/bryan_000/Documents/GitHub/Data/UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("C:/Users/bryan_000/Documents/GitHub/Data/UCI HAR Dataset/train/subject_train.txt")
 names(X_train) = features
 
 # Extract only the measurements on the mean and standard deviation for each measurement.
@@ -73,4 +71,4 @@ melt_data      = melt(data, id = id_labels, measure.vars = data_labels)
 # Apply mean function to dataset using dcast function
 tidy_data   = dcast(melt_data, subject + Activity_Label ~ variable, mean)
 
-write.table(tidy_data, file = "./tidydata.txt")
+write.table(tidy_data, file = "C:/Users/bryan_000/Documents/GitHub/Data/tidydata.txt")
